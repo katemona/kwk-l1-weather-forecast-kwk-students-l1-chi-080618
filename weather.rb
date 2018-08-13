55d1bff1515ede5e08d7586625bec573b87d7def
katemona <k.monahan@mothermcauley.org>
2018-08-13 14:21:42 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -18,8 +18,10 @@ weather_two = {
 def location(hash)
   puts hash[:city]
 end
+
 location(weather)
 location(weather_two)
+
 # 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash. 
 
 def description(weather)
