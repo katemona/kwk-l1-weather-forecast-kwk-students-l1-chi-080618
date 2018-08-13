01e8472d2ee60f2b221776dd9f69f2d7ab8f7a4b
katemona <k.monahan@mothermcauley.org>
2018-08-13 14:18:56 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -8,34 +8,20 @@ weather = {
 
 # 1. Complete the method below called location, that accepts a hash called weather as an argument so that it returns the value of city from the weather hash. 
 
-def location(weather)
-  weather = {
-	:city => "San Francisco",
-	:description => "Sunny",
-	:temperature_farenheit => 75
-  }
-  puts weather[:city]
+def location(hash)
+  return weather[:city]
 end
+location(weather)
 
 # 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash. 
 
 def description(weather)
-  weather = {
-	:city => "San Francisco",
-	:description => "Sunny",
-	:temperature_farenheit => 75
-  }
   puts weather[:description]
 end
 
 # 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.1 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal. 
 
 def interp(weather)
-  weather = {
-	:city => "New York",
-	:description => "Partly Cloudy",
-	:temperature_farenheit => 75
-  } 
   return "The weather in #{:city} is #{:description}. the temperature is #{:temperature_farenheit}."
 end
 
