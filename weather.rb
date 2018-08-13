2ff1969953f30530712f2da2a25384661a2c783f
katemona <k.monahan@mothermcauley.org>
2018-08-13 14:22:30 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -16,7 +16,7 @@ weather_two = {
 # 1. Complete the method below called location, that accepts a hash called weather as an argument so that it returns the value of city from the weather hash. 
 
 def location(hash)
-  puts hash[:city]
+  return hash[:city]
 end
 
 location(weather)
